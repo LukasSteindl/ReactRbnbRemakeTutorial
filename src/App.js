ab7877901e9b1335fd85ff70d1e74756920bb3f1
LukasSteindl <lukassteindl@outlook.com>
2018-02-03 17:13:40 -0800

    sehrgut

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       Hallo Welt das ist der nächste version
+       Hallo Welt das ist der version
       </div>
     );
   }
