72fd4aa64eb0c93575b0045810d89c6963aa2e8b
LukasSteindl <lukassteindl@outlook.com>
2018-02-04 20:37:46 -0800

    rbnb clone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-
+import Flat from './components/Flat';
+import "./app.css";
 class App extends Component {
   render() {
+    const flat ={
+      "name": "Super 60m2 in trendy neighborhood!",
+      "imageUrl" : 
+        "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg",
+      "price": 150,
+      "priceCurrency": "EUR",
+      "lat": 48.885312,
+      "lng": 2.341225
+    };
+
     return (
-      <div className="App">
-       Hallo Welt das ist derasdf version
+      <div className="app">
+        <div className="main">
+          <div className="search"></div>
+          <div className="flats">
+          <Flat flat={flat}/>
+          <Flat flat={flat}/>
+          </div>
+        </div>
+        <div className="map"> </div>
       </div>
     );
   }
---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hello extends Component
+{
+    render()
+    {
+        return <div> Hallo test 123 {this.props.Name} </div>;
+    }
+}
+
+export default Hello;
